8994799f8bbe3cc51d4d5c190f34f9a7f7215091
Julia <matterfixed@gmail.com>
2023-06-28 18:30:34 +0300

    done

---
+++ hw7/view/View.java
@@ -0,0 +1,15 @@
+package hw7.view;
+
+import java.util.Scanner;
+
+public class View {
+    Scanner in = new Scanner(System.in);
+    public double inputValue(String title){
+        System.out.printf("%s", title);
+        return in.nextDouble();
+    }
+    public char inputOperationSign(String title){
+        System.out.printf("%s", title);
+        return in.next(".").charAt(0);
+    }
+}
